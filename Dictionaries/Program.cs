7343cb9830e160c70adc1e14525bc847da24cc47
uvekt <ST10451306@VCCONNECT.edu.za>
2026-03-19 15:31:39 +0200

    WorkFlow

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Dictionaries are collections of data stored as key value pairs 
+             * 
+             * -> provides fast data access based on a unique key 
+             * 
+             * 
+             * 
+             * dictionary Dict<key Value> = new Dictionary<>(
+             * {
+             * {"key", "Value}
+             * }
+             * */
+
+            Dictionary<string, string> valuepairs = new Dictionary<string, string>();
+
+            // adding value
+            // to delete you set the value to a string 
+            // garbage collection usetting values or deleting the memory 
+
+            valuepairs.Add("Computer Science", "Programming");
+            valuepairs.Add("Law", "Criminology");
+            valuepairs.Add("Education", "Teaching Practice");
+
+            // add via index 
+            valuepairs["Engineering"] = "Mathamatics";  // use this code  to add to the dictionary 
+
+            // set the value to "delete it " this is setting the value to null 
+            valuepairs["Education"] = "";
+
+            // printing 
+            foreach (KeyValuePair<string, string> pair in valuepairs)
+            {
+                Console.WriteLine($"Courses: {pair.Key} | Module {pair.Value}");  
+            }
+
+
+            // check if a specific dict key is contained a key 
+            bool containsKey = valuepairs.ContainsKey("Education");
+            if (containsKey)
+            {
+               Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+ }
+            // remove to delete 
+            valuepairs.Remove("Education");
+                    //printing 
+                    foreach (KeyValuePair<string, string> pair in valuepairs) ;
+
+                    //return the number of items in the dict 
+
+                    int items = valuepairs.Count;
+
+            Console.WriteLine(items);
+
+            //TryGetValue to see if the value exists 
+            valuepairs.Clear();
+
+            int itemsb = valuepairs.Count;
+            Console.WriteLine(items);
+           
+        }
+    }
+}
---
+++ Dictionary2/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Dictionary2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create dictionaries with user input 
+
+            var courses = new Dictionary<string, List<string>>();
+
+            // accept user input 
+            while (true)
+            {
+
+                Console.WriteLine("Enter Program Name or 'Stop to Exit: ");
+                string key = Console.ReadLine();
+
+                if (key?.ToLower() == "stop") break;
+
+                Console.WriteLine($"Enter Modules for {key}: ");
+                string module = Console.ReadLine();
+
+                if (!courses.ContainsKey(key))
+                {
+                    courses[key] = new List<string>();
+                }
+                courses[key].Add(module);
+
+                
+            }
+            foreach (var entry in courses)
+            {
+                Console.WriteLine($"Courses: {entry.Key} | Module: {string.Join(",", entry.Value)}");
+            }
+        }
+    }
+}
+
+/* exercise 
+ * create a dictionary that stores employee details as well as there id 
+ * create an employee dictionary <int ID, EmpID , List<Employee>>() 
+ * 
+ * The dictionary will store the following details 
+ *  emp number  and emp Details 
+ *  
+ *  
+ * 1 Employee Details are from a seperate class Employee are as followes
+ *      1 EmpID
+ *      2 EmpName
+ *      3 EmpSurname 
+ *      4 EmpSalary
+ *      5 EmpPosition 
+ *      
+ *      2 Create the method searchEmp(int EmpID)
+ *      That searches for an employee using EmpID 
+ *      
+ *      3 create a methods searchEmpDetails(string value)
+ *      that allows searching the dictionary usning employee details 
+ *      this method must return all matches from the List<Employee>
+ *       
+ * 
+ */
